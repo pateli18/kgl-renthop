bb4cb84f3c75bcb082aaba37e872f371992b49d5
Ihsaan Patel <ihsaan.patel@gmail.com>
2017-03-16 11:10:28 -0500

    Fixed clean file (store strings with commas as lists)

--- clean_dataset.py
+++ clean_dataset.py
@@ -3,17 +3,16 @@ import sys
 
 def clean_data(filename, new_filename):
 	df = pd.read_json(filename)
-	columns_to_encode = ['description', 'display_address', 'street_address']
-	for column in columns_to_encode:
-		df[column] = df[column].apply(lambda value: value.encode('ascii', 'ignore').decode('ascii'))
-	print("Original Shape: {0} by {1}".format(df.shape[0], df.shape[0]))
+	print("Original Shape: {0}".format(df.shape))
 	# drop duplicate columns
 	duplicate_columns = ['bathrooms', 'bedrooms', 'building_id', 'description', 'display_address', 'latitude', 'longitude', 'manager_id', 'price', 'street_address']
 	df_deduped = df.drop_duplicates(duplicate_columns)
 	# drop price outliers
 	df_deduped = df_deduped[(df_deduped['price'] > 50) & (df_deduped['price'] < 1000000)]
-	df_deduped['description'] = df_deduped['description'].apply(lambda description: [description])
-	print("New Shape: {0} by {1}".format(df_deduped.shape[0], df_deduped.shape[0]))
+	columns_to_encode = ['description', 'display_address', 'street_address']
+	for column in columns_to_encode:
+		df_deduped[column] = df_deduped[column].apply(lambda value: [value.encode('ascii', 'ignore')])
+	print("New Shape: {0}".format(df_deduped.shape))
 	df_deduped.to_csv(new_filename, index = False)
 	print("Complete")
 
