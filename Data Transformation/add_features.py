6a39c4c8c01432ad12c6f050319e6c22172a1299
Ihsaan Patel <ihsaan.patel@gmail.com>
2017-03-18 18:41:50 -0500

    altered and renamed add_feature_lengths file to include adding date attributes based on the created parameter

--- Data Transformation/add_features.py
+++ Data Transformation/add_features.py
@@ -2,17 +2,26 @@ import pandas as pd
 import numpy as np
 from ast import literal_eval
 import sys
+import datetime
 
-def add_lengths(load_filepath, save_filepath):
+def add_features(load_filepath, save_filepath):
 	df = pd.read_csv(load_filepath)
+	# add feature lengths
 	df['description_length'] = df['description'].apply(lambda x: len(literal_eval(x)[0]))
 	df['num_photos'] = df['photos'].apply(lambda x: len(x))
 	df['num_features'] = df['features'].apply(lambda x: len(x))
+	# add non-linear prices
 	df['price_sq'] = df['price'].apply(lambda x: x**2)
 	df['price_log'] = df['price'].apply(lambda x: np.log(x))
+	# add date features
+	df['created'] = pd.to_datetime(df['created'],infer_datetime_format = True)
+	df['month'] = df['created'].apply(lambda date: date.month)
+	df['day'] = df['created'].apply(lambda date: date.day)
+	df['dayofweek'] = df['created'].apply(lambda date: date.dayofweek)
+	df['hour'] = df['created'].apply(lambda date: date.hour)
 	df.to_csv(save_filepath, index = False)
 	print("Complete")
 
 load_filepath = sys.argv[1]
 save_filepath = sys.argv[2]
-add_lengths(load_filepath, save_filepath)
+add_features(load_filepath, save_filepath)
